617476823c444b216ba47324a6d3bdd1f348639a
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-15 18:04:58 +0300

    Explain character topic workflow

--- velvet_bot/handlers/start.py
+++ velvet_bot/handlers/start.py
@@ -15,11 +15,12 @@ async def handle_start(message: Message, bot_username: str) -> None:
 
     await message.answer(
         f"<b>Velvet Archive</b>{greeting}\n\n"
-        "Создать персонажа: <code>/create Каин</code>\n"
+        "Создать персонажа и назначить тему:\n"
+        "<code>/create Аид https://t.me/c/3951213065/1398</code>\n\n"
+        "Назначить тему существующему персонажу:\n"
+        "<code>/topic Аид https://t.me/c/3951213065/1398</code>\n\n"
         "Показать персонажей: <code>/characters</code>\n"
-        "Открыть профиль: <code>/character Каин</code>\n\n"
-        "Чтобы сохранить изображение из любого чата, ответьте на него и отправьте:\n"
-        f"<code>@{safe_username} save Каин</code>\n"
-        "или\n"
-        f"<code>/save@{safe_username} Каин</code>"
+        "Открыть профиль: <code>/character Аид</code>\n\n"
+        "Чтобы сохранить фото или видео из любого чата, ответьте на него и отправьте:\n"
+        f"<code>@{safe_username} save Аид</code>"
     )
