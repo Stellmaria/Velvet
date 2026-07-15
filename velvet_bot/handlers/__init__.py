87366866f6d9ea3d7a01691cacaa109fd9bb6a41
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-15 20:01:20 +0300

    Register character reference handlers

--- velvet_bot/handlers/__init__.py
+++ velvet_bot/handlers/__init__.py
@@ -9,6 +9,7 @@ from velvet_bot.handlers.characters import router as characters_router
 from velvet_bot.handlers.guest_archive import router as guest_archive_router
 from velvet_bot.handlers.inline_help import router as inline_help_router
 from velvet_bot.handlers.media_browser import router as media_browser_router
+from velvet_bot.handlers.references import router as references_router
 from velvet_bot.handlers.start import router as start_router
 
 logger = logging.getLogger(__name__)
@@ -42,6 +43,7 @@ async def handle_unhandled_error(
 router.include_router(start_router)
 router.include_router(characters_router)
 router.include_router(media_browser_router)
+router.include_router(references_router)
 router.include_router(inline_help_router)
 router.include_router(guest_archive_router)
 router.include_router(archive_router)
