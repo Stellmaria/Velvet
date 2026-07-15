9926554360b824b4f037b732156817c5fff45ed8
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-15 18:29:25 +0300

    Route Guest Mode through external reply handler first

--- velvet_bot/handlers/__init__.py
+++ velvet_bot/handlers/__init__.py
@@ -2,11 +2,13 @@ from aiogram import Router
 
 from velvet_bot.handlers.archive import router as archive_router
 from velvet_bot.handlers.characters import router as characters_router
+from velvet_bot.handlers.guest_archive import router as guest_archive_router
 from velvet_bot.handlers.start import router as start_router
 
 router = Router(name=__name__)
 router.include_router(start_router)
 router.include_router(characters_router)
+router.include_router(guest_archive_router)
 router.include_router(archive_router)
 
 __all__ = ("router",)
