44eaa4b2516a2433931503226edefbb44c0037ec
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-16 14:10:04 +0300

    refactor: вынести stories constants

---
+++ velvet_bot/domains/stories/constants.py
@@ -0,0 +1,11 @@
+STORY_REQUIRED_UNIVERSES = frozenset({"shs", "kr", "lm", "idm", "lagerta"})
+KNOWN_UNIVERSES = frozenset(
+    {"shs", "kr", "lm", "idm", "bg3", "lagerta", "original"}
+)
+RELEASE_PRECISIONS = frozenset({"day", "month", "year", "unknown"})
+
+__all__ = (
+    "KNOWN_UNIVERSES",
+    "RELEASE_PRECISIONS",
+    "STORY_REQUIRED_UNIVERSES",
+)
