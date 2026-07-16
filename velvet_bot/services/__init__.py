0ba9e02df5a54e7beb4a9cc2bfa69e6d29d72a49
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-16 10:00:28 +0300

    Добавить пакет сервисов

---
+++ velvet_bot/services/__init__.py
@@ -0,0 +1,7 @@
+from velvet_bot.services.system_health import (
+    DiskSnapshot,
+    SystemHealthReport,
+    SystemHealthService,
+)
+
+__all__ = ("DiskSnapshot", "SystemHealthReport", "SystemHealthService")
