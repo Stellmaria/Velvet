78babf4ada52170e80f4dc2d2b3c3d5581e0e441
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-16 14:15:56 +0300

    refactor: экспортировать reference media adapter

--- velvet_bot/infrastructure/telegram/__init__.py
+++ velvet_bot/infrastructure/telegram/__init__.py
@@ -1,4 +1,9 @@
 from velvet_bot.infrastructure.telegram.publication_delivery import TelegramPublicationDelivery
 from velvet_bot.infrastructure.telegram.publication_inbox import publication_payload_from_message
+from velvet_bot.infrastructure.telegram.reference_media import reference_payload_from_photo
 
-__all__ = ("TelegramPublicationDelivery", "publication_payload_from_message")
+__all__ = (
+    "TelegramPublicationDelivery",
+    "publication_payload_from_message",
+    "reference_payload_from_photo",
+)
