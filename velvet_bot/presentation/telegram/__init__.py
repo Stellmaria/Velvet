2a3d14a8691a74d6bfb33e9c2112bd57c4dcb9e1
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-16 13:31:24 +0300

    fix: сделать Telegram presentation package ленивым

--- velvet_bot/presentation/telegram/__init__.py
+++ velvet_bot/presentation/telegram/__init__.py
@@ -1,3 +1,15 @@
-from velvet_bot.presentation.telegram.router import get_root_router
+from __future__ import annotations
+
+from typing import Any
 
 __all__ = ("get_root_router",)
+
+
+def __getattr__(name: str) -> Any:
+    if name != "get_root_router":
+        raise AttributeError(name)
+
+    from velvet_bot.presentation.telegram.router import get_root_router
+
+    globals()[name] = get_root_router
+    return get_root_router
