c39d7a6044926395f8d81db7f132b9f6b1633f79
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-16 10:22:08 +0300

    Экспортировать репозиторий публикаций

--- velvet_bot/repositories/__init__.py
+++ velvet_bot/repositories/__init__.py
@@ -1,6 +1,11 @@
+from velvet_bot.repositories.publication_repository import PublicationRepository
 from velvet_bot.repositories.system_repository import (
     RuntimeDatabaseSnapshot,
     SystemRepository,
 )
 
-__all__ = ("RuntimeDatabaseSnapshot", "SystemRepository")
+__all__ = (
+    "PublicationRepository",
+    "RuntimeDatabaseSnapshot",
+    "SystemRepository",
+)
