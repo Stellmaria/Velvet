41ebfcb620f71cea4778ac0abd223bad1f0f7bc1
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-16 04:52:14 +0300

    Убрать неиспользуемую подмену метода базы

--- velvet_bot/media_preview_persistence.py
+++ velvet_bot/media_preview_persistence.py
@@ -1,12 +1,6 @@
 from __future__ import annotations
 
-from typing import Any
-
-from velvet_bot.database import Database, SaveMediaResult
-from velvet_bot.media import MediaDescriptor
-
-_INSTALLED = False
-_ORIGINAL_SAVE = Database.save_character_media
+from velvet_bot.database import Database
 
 
 async def set_media_preview(
@@ -38,31 +32,3 @@ async def set_media_preview(
             height,
             source,
         )
-
-
-async def _save_with_preview(
-    self: Database,
-    character,
-    media: MediaDescriptor,
-    **kwargs: Any,
-) -> SaveMediaResult:
-    result = await _ORIGINAL_SAVE(self, character, media, **kwargs)
-    if media.preview_file_id:
-        await set_media_preview(
-            self,
-            media_id=result.media_id,
-            file_id=media.preview_file_id,
-            file_unique_id=media.preview_file_unique_id,
-            width=media.preview_width,
-            height=media.preview_height,
-            source=media.preview_source or "source_thumbnail",
-        )
-    return result
-
-
-def install_media_preview_persistence() -> None:
-    global _INSTALLED
-    if _INSTALLED:
-        return
-    Database.save_character_media = _save_with_preview  # type: ignore[method-assign]
-    _INSTALLED = True
