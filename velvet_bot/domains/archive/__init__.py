0e94840de0cf8a630a5c6048e4c4c464dafe01d7
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-16 14:25:08 +0300

    refactor: экспортировать archive preview domain API

--- velvet_bot/domains/archive/__init__.py
+++ velvet_bot/domains/archive/__init__.py
@@ -1,11 +1,16 @@
 from velvet_bot.domains.archive.models import ArchivePage, ArchivedMedia, DeletedArchiveItem
+from velvet_bot.domains.archive.preview_models import PreviewPayload, PreviewRecord
+from velvet_bot.domains.archive.preview_repository import ArchivePreviewRepository
 from velvet_bot.domains.archive.repository import ArchiveRepository
 from velvet_bot.domains.archive.service import ArchiveService
 
 __all__ = (
     "ArchivePage",
+    "ArchivePreviewRepository",
     "ArchiveRepository",
     "ArchiveService",
     "ArchivedMedia",
     "DeletedArchiveItem",
+    "PreviewPayload",
+    "PreviewRecord",
 )
