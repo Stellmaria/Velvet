f0cef90962bebf1d19a10d5fbe7d39688afafa7b
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-16 14:25:21 +0300

    refactor: экспортировать archive preview Telegram adapter

--- velvet_bot/infrastructure/telegram/__init__.py
+++ velvet_bot/infrastructure/telegram/__init__.py
@@ -1,8 +1,12 @@
+from velvet_bot.infrastructure.telegram.archive_previews import (
+    TelegramArchivePreviewResolver,
+)
 from velvet_bot.infrastructure.telegram.publication_delivery import TelegramPublicationDelivery
 from velvet_bot.infrastructure.telegram.publication_inbox import publication_payload_from_message
 from velvet_bot.infrastructure.telegram.reference_media import reference_payload_from_photo
 
 __all__ = (
+    "TelegramArchivePreviewResolver",
     "TelegramPublicationDelivery",
     "publication_payload_from_message",
     "reference_payload_from_photo",
