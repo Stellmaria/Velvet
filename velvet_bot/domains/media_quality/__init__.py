68102eb4a552d08ed52d7ab573f7492cec2ebd29
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-16 13:39:25 +0300

    refactor: экспортировать duplicate review domain models

--- velvet_bot/domains/media_quality/__init__.py
+++ velvet_bot/domains/media_quality/__init__.py
@@ -1,4 +1,6 @@
 from velvet_bot.domains.media_quality.models import (
+    DuplicateCandidate,
+    DuplicatePage,
     MediaFileCheckTarget,
     MediaQualityRunResult,
     MediaScanTarget,
@@ -8,6 +10,8 @@ from velvet_bot.domains.media_quality.repository import MediaQualityRepository
 from velvet_bot.domains.media_quality.service import MediaQualityService
 
 __all__ = (
+    "DuplicateCandidate",
+    "DuplicatePage",
     "MediaFileCheckTarget",
     "MediaQualityRepository",
     "MediaQualityRunResult",
