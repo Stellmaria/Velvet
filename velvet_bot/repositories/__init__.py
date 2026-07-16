5a6f34e569aff20e3d866369fbe11aff98683ca6
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-16 10:25:11 +0300

    Экспортировать репозиторий уведомлений

--- velvet_bot/repositories/__init__.py
+++ velvet_bot/repositories/__init__.py
@@ -1,3 +1,7 @@
+from velvet_bot.repositories.public_notification_repository import (
+    PendingPublicNotification,
+    PublicNotificationRepository,
+)
 from velvet_bot.repositories.publication_repository import PublicationRepository
 from velvet_bot.repositories.system_repository import (
     RuntimeDatabaseSnapshot,
@@ -5,6 +9,8 @@ from velvet_bot.repositories.system_repository import (
 )
 
 __all__ = (
+    "PendingPublicNotification",
+    "PublicNotificationRepository",
     "PublicationRepository",
     "RuntimeDatabaseSnapshot",
     "SystemRepository",
