608ddb1da7f48c34e2e91e0567aca6521b93352b
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-16 14:15:18 +0300

    refactor: экспортировать references domain API

---
+++ velvet_bot/domains/references/__init__.py
@@ -0,0 +1,19 @@
+from velvet_bot.domains.references.models import (
+    AddReferenceResult,
+    CharacterReference,
+    DeleteReferenceResult,
+    ReferenceMediaPayload,
+    ReferencePage,
+)
+from velvet_bot.domains.references.repository import ReferenceRepository
+from velvet_bot.domains.references.service import ReferenceService
+
+__all__ = (
+    "AddReferenceResult",
+    "CharacterReference",
+    "DeleteReferenceResult",
+    "ReferenceMediaPayload",
+    "ReferencePage",
+    "ReferenceRepository",
+    "ReferenceService",
+)
