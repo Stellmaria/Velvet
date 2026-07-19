99e0251a9f8f5b6ed450e8f227653b943053d19e
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-19 15:48:27 +0300

    Use canonical Supervisor and System routers

--- velvet_bot/presentation/telegram/routers/core_operations.py
+++ velvet_bot/presentation/telegram/routers/core_operations.py
@@ -5,8 +5,10 @@ from aiogram import Router
 from velvet_bot.handlers.error_center import router as error_center_router
 from velvet_bot.handlers.owner_actions import router as owner_actions_router
 from velvet_bot.handlers.owner_menu import router as owner_menu_router
-from velvet_bot.handlers.supervisor_control import router as supervisor_control_router
-from velvet_bot.handlers.system_center import router as system_center_router
+from velvet_bot.presentation.telegram.routers.supervisor.control import (
+    router as supervisor_control_router,
+)
+from velvet_bot.presentation.telegram.routers.system import router as system_center_router
 
 router = Router(name=__name__)
 router.include_router(error_center_router)
