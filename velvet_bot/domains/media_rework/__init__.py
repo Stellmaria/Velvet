53d4fba4c5a47d70e77c10f01bf4011ed36ee3bc
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-20 22:12:55 +0300

    Expose media rework domain

---
+++ velvet_bot/domains/media_rework/__init__.py
@@ -0,0 +1,13 @@
+from velvet_bot.domains.media_rework.repository import (
+    MediaReworkItem,
+    MediaReworkPage,
+    MediaReworkRepository,
+    MediaReworkSummary,
+)
+
+__all__ = (
+    "MediaReworkItem",
+    "MediaReworkPage",
+    "MediaReworkRepository",
+    "MediaReworkSummary",
+)
