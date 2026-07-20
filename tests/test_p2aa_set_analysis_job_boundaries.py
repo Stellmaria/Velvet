951875fa523d6ebdbed09dc17f2c79c25c0b8b1c
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-20 16:04:13 +0300

    Use canonical quality set AI module in boundary tests

--- tests/test_p2aa_set_analysis_job_boundaries.py
+++ tests/test_p2aa_set_analysis_job_boundaries.py
@@ -4,7 +4,7 @@ import asyncio
 import unittest
 from types import SimpleNamespace
 
-import velvet_bot.handlers.quality_set_ai as module
+import velvet_bot.presentation.telegram.routers.quality_operations_controllers.quality_set_ai as module
 
 
 class FakeMessage:
