de5820b4c5c9ca1852c397a634e734fc154ad5b2
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-21 12:56:35 +0300

    Export media set domain repository

---
+++ velvet_bot/domains/media_sets/__init__.py
@@ -0,0 +1,9 @@
+from velvet_bot.domains.media_sets.repository import (
+    MediaSetCandidateIdPage,
+    MediaSetCandidateListingRepository,
+)
+
+__all__ = (
+    "MediaSetCandidateIdPage",
+    "MediaSetCandidateListingRepository",
+)
