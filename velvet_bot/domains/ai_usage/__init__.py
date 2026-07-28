7084db4f64d21930aa1492dbb63a484b8176e553
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-28 23:09:57 +0300

    Экспортировать owner-контракты AI usage

--- velvet_bot/domains/ai_usage/__init__.py
+++ velvet_bot/domains/ai_usage/__init__.py
@@ -1,22 +1,36 @@
+from .factory import build_ai_usage_service
 from .ledger import AIUsageRepository
 from .models import (
+    AIBudgetStatus,
+    AIBudgetWarning,
     AIProviderResult,
     AIRequestContext,
     AIReservation,
+    AIUsageEvent,
     AIUsageTotals,
 )
 from .pricing import AITokenPricing, load_token_pricing
-from .service import AIBudgetExceeded, AIRequestExecutor, AIUsageService
+from .service import (
+    AIBudgetExceeded,
+    AIRequestExecutor,
+    AIUsageService,
+    BudgetWarningHandler,
+)
 
 __all__ = (
     "AIBudgetExceeded",
+    "AIBudgetStatus",
+    "AIBudgetWarning",
     "AIProviderResult",
     "AIRequestContext",
     "AIRequestExecutor",
     "AIReservation",
     "AITokenPricing",
+    "AIUsageEvent",
     "AIUsageRepository",
     "AIUsageService",
     "AIUsageTotals",
+    "BudgetWarningHandler",
+    "build_ai_usage_service",
     "load_token_pricing",
 )
