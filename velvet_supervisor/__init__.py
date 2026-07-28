c47f924cc354cb5ff45a913726853c4e5a628e9a
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-28 14:20:51 +0300

    Attach hardware profile to Ollama status

--- velvet_supervisor/__init__.py
+++ velvet_supervisor/__init__.py
@@ -1,4 +1,7 @@
 from .config import SupervisorSettings
+from .hardware_profile import install_ollama_status_hardware_hook
 from .runtime import VelvetSupervisor
+
+install_ollama_status_hardware_hook()
 
 __all__ = ("SupervisorSettings", "VelvetSupervisor")
