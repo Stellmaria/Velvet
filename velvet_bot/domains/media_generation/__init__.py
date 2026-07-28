b6b8aac76456af578f96e5fd81a78a2514656a4d
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-29 01:07:38 +0300

    Экспортировать модели генерации Kie

---
+++ velvet_bot/domains/media_generation/__init__.py
@@ -0,0 +1,17 @@
+from .models import (
+    KieGenerationRequest,
+    KieModelAlias,
+    KieModelCatalog,
+    KiePricing,
+    KieTaskRecord,
+    KieTaskState,
+)
+
+__all__ = (
+    "KieGenerationRequest",
+    "KieModelAlias",
+    "KieModelCatalog",
+    "KiePricing",
+    "KieTaskRecord",
+    "KieTaskState",
+)
