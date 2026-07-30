07000b189c1991fd677030bf6e0ceb2920be1a17
Stellmaria <aam.cheshire@gmail.com>
2026-07-30 07:01:58 +0300

    Восстановить legacy alias runtime installer

--- velvet_bot/app/meow_runtime_install.py
+++ velvet_bot/app/meow_runtime_install.py
@@ -2,9 +2,9 @@
 
 from velvet_bot.app.auf_runtime_install import install_auf_runtime_dispatcher
 
-install_auf_runtime_dispatcher = install_auf_runtime_dispatcher
+install_meow_runtime_dispatcher = install_auf_runtime_dispatcher
 
 __all__ = (
     "install_auf_runtime_dispatcher",
-    "install_auf_runtime_dispatcher",
+    "install_meow_runtime_dispatcher",
 )
