50d05363a879a49e9a7031c2f45587a8b234661c
Anastasia Melnikova <aam.cheshire@gmail.com>
2026-07-30 05:14:24 +0300

    Расширить канонический API runtime Ауф

--- velvet_bot/domains/auf_runtime/__init__.py
+++ velvet_bot/domains/auf_runtime/__init__.py
@@ -15,6 +15,8 @@ from velvet_bot.domains.meow_runtime import (
     MeowRuntimeSettings,
     WorkspaceMeowSettings,
 )
+from velvet_bot.domains.meow_runtime.dispatcher import MeowGenerationDispatcher
+from velvet_bot.domains.meow_runtime.queue import ProviderMeowTaskQueueService
 
 # Persistent keys cannot be renamed independently from existing workspace rows and
 # already-sent Telegram keyboards. Keep them in one explicit compatibility boundary.
@@ -22,23 +24,27 @@ AUF_MODULE_KEY = "meow"
 AUF_WORKSPACE_ACTION = "meow"
 
 AufCancellationResult = MeowCancellationResult
+AufGenerationDispatcher = MeowGenerationDispatcher
 AufProvider = MeowProvider
 AufProviderSnapshot = MeowProviderSnapshot
 AufRuntimeAccessError = MeowRuntimeAccessError
 AufRuntimeRepository = MeowRuntimeRepository
 AufRuntimeService = MeowRuntimeService
 AufRuntimeSettings = MeowRuntimeSettings
+ProviderAufTaskQueueService = ProviderMeowTaskQueueService
 WorkspaceAufSettings = WorkspaceMeowSettings
 
 __all__ = (
     "AUF_MODULE_KEY",
     "AUF_WORKSPACE_ACTION",
     "AufCancellationResult",
+    "AufGenerationDispatcher",
     "AufProvider",
     "AufProviderSnapshot",
     "AufRuntimeAccessError",
     "AufRuntimeRepository",
     "AufRuntimeService",
     "AufRuntimeSettings",
+    "ProviderAufTaskQueueService",
     "WorkspaceAufSettings",
 )
