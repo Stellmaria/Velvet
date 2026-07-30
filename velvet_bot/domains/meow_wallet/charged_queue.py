a5a629eda22a1405b5be6686fa459d2b1286b9b9
Stellmaria <aam.cheshire@gmail.com>
2026-07-30 07:02:48 +0300

    Восстановить legacy charged queue factory

--- velvet_bot/domains/meow_wallet/charged_queue.py
+++ velvet_bot/domains/meow_wallet/charged_queue.py
@@ -3,8 +3,8 @@ from velvet_bot.domains.auf_wallet.charged_queue import (
     AufChargedTaskQueueService, build_auf_charged_task_queue_service,
 )
 MeowChargedTaskQueueService = AufChargedTaskQueueService
-build_auf_charged_task_queue_service = build_auf_charged_task_queue_service
+build_meow_charged_task_queue_service = build_auf_charged_task_queue_service
 __all__ = (
     "MeowChargedTaskQueueService",
-    "build_auf_charged_task_queue_service",
+    "build_meow_charged_task_queue_service",
 )
