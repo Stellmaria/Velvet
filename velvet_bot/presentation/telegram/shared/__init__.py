0f70a14e891030d70ca77770c0d3da6c47c828b5
Stellmaria <aam.cheshire@gmail.com>
2026-07-30 16:52:48 +0300

    P3: expose deletion and retry contracts

--- velvet_bot/presentation/telegram/shared/__init__.py
+++ velvet_bot/presentation/telegram/shared/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from .deletion import delete_message_safely
+from .deletion import delete_message_safely, is_message_already_absent
 from .editing import (
     is_message_not_modified,
     safe_edit_callback_text,
@@ -13,6 +13,7 @@ from .navigation import (
     build_navigation_keyboard,
     build_pagination_keyboard,
 )
+from .retry import TelegramRetryPolicy, retry_telegram_operation
 from .text import (
     TELEGRAM_TEXT_CHUNK_LIMIT,
     answer_text_chunks,
@@ -22,6 +23,7 @@ from .text import (
 __all__ = (
     "NavigationButton",
     "TELEGRAM_TEXT_CHUNK_LIMIT",
+    "TelegramRetryPolicy",
     "answer_text_chunks",
     "build_back_refresh_keyboard",
     "build_navigation_keyboard",
@@ -29,7 +31,9 @@ __all__ = (
     "chunk_telegram_text",
     "delete_message_safely",
     "download_telegram_file",
+    "is_message_already_absent",
     "is_message_not_modified",
+    "retry_telegram_operation",
     "safe_edit_callback_text",
     "safe_edit_message_text",
 )
