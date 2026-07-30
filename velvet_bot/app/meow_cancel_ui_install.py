d66cc1ad5d0a6c7d301ec479e85bc48682bb8b0f
Stellmaria <aam.cheshire@gmail.com>
2026-07-30 07:01:37 +0300

    Восстановить legacy alias установщика отмены

--- velvet_bot/app/meow_cancel_ui_install.py
+++ velvet_bot/app/meow_cancel_ui_install.py
@@ -2,9 +2,9 @@
 
 from velvet_bot.app.auf_cancel_ui_install import install_auf_cancel_ui
 
-install_auf_cancel_ui = install_auf_cancel_ui
+install_meow_cancel_ui = install_auf_cancel_ui
 
 __all__ = (
     "install_auf_cancel_ui",
-    "install_auf_cancel_ui",
+    "install_meow_cancel_ui",
 )
