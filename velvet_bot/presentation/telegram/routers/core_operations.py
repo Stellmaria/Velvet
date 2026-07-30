dd2be3350e02293874404dd77a562623a20017d6
Stellmaria <aam.cheshire@gmail.com>
2026-07-30 21:06:15 +0300

    Подключить управление пользователями через core operations

--- velvet_bot/presentation/telegram/routers/core_operations.py
+++ velvet_bot/presentation/telegram/routers/core_operations.py
@@ -24,10 +24,14 @@ from velvet_bot.presentation.telegram.routers.supervisor.control import (
     router as supervisor_control_router,
 )
 from velvet_bot.presentation.telegram.routers.system import router as system_center_router
+from velvet_bot.presentation.telegram.routers.user_management import (
+    router as user_management_router,
+)
 
 router = Router(name=__name__)
 router.include_router(error_center_router)
 router.include_router(diagnostics_router)
+router.include_router(user_management_router)
 router.include_router(owner_actions_router)
 router.include_router(owner_help_router)
 router.include_router(workspace_admin_panel_router)
