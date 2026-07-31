9146aad2a51f68e4e74ba965b52ed13f255e11e3
Stellmaria <aam.cheshire@gmail.com>
2026-08-01 02:02:21 +0300

    Экспортировать typed media delivery errors

--- velvet_bot/application/media_delivery.py
+++ velvet_bot/application/media_delivery.py
@@ -1,6 +1,19 @@
 from __future__ import annotations
 
 from velvet_bot.application.media_delivery_deliver import DeliverMediaResult
+from velvet_bot.application.media_delivery_errors import (
+    MediaDeliveryFailure,
+    MediaDeliveryFailureKind,
+    MediaDeliveryInvariantError,
+    MediaDeliveryRecordedError,
+    MediaDeliveryTerminalError,
+    MediaDeliveryTransientError,
+    classify_media_delivery_error,
+    media_delivery_error_fields,
+    media_delivery_error_text,
+    raise_if_programming_error,
+    recorded_media_delivery_error,
+)
 from velvet_bot.application.media_delivery_models import (
     DownloadedMedia,
     MediaDeliveryItem,
@@ -17,8 +30,28 @@ from velvet_bot.application.media_delivery_redeliver import RedeliverMediaResult
 from velvet_bot.application.media_delivery_resolve import ResolveProviderResult
 
 __all__ = (
-    "DeliverMediaResult", "DownloadedMedia", "MediaDeliveryItem", "MediaDeliveryJob",
-    "MediaDeliveryRepository", "MediaDeliveryStatus", "MediaDeliveryStepStatus",
-    "MediaDeliverySummary", "MediaDeliveryTransport", "MediaUrlExpired",
-    "ProviderResultResolver", "RedeliverMediaResult", "ResolveProviderResult",
+    "DeliverMediaResult",
+    "DownloadedMedia",
+    "MediaDeliveryFailure",
+    "MediaDeliveryFailureKind",
+    "MediaDeliveryInvariantError",
+    "MediaDeliveryItem",
+    "MediaDeliveryJob",
+    "MediaDeliveryRecordedError",
+    "MediaDeliveryRepository",
+    "MediaDeliveryStatus",
+    "MediaDeliveryStepStatus",
+    "MediaDeliverySummary",
+    "MediaDeliveryTerminalError",
+    "MediaDeliveryTransientError",
+    "MediaDeliveryTransport",
+    "MediaUrlExpired",
+    "ProviderResultResolver",
+    "RedeliverMediaResult",
+    "ResolveProviderResult",
+    "classify_media_delivery_error",
+    "media_delivery_error_fields",
+    "media_delivery_error_text",
+    "raise_if_programming_error",
+    "recorded_media_delivery_error",
 )
