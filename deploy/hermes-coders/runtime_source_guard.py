3ac0e3370561303eb0c8ff9d29e0a05862555a50
Stellmaria <aam.cheshire@gmail.com>
2026-08-06 18:54:40 +0300

    Guard Codex image limit preflight runtime source

--- deploy/hermes-coders/runtime_source_guard.py
+++ deploy/hermes-coders/runtime_source_guard.py
@@ -19,6 +19,7 @@ RUNTIME_SOURCES = (
     "codex_image_runner.py",
     "byesu_image_fallback.py",
     "byesu_image_routing_policy.py",
+    "codex_image_limit_preflight.py",
     "codex_launcher_runner.py",
     "codex_context_launcher_runner.py",
     "sandbox_launcher_client.py",
@@ -35,6 +36,7 @@ import codex_tier_runner
 import codex_image_runner
 import byesu_image_fallback
 import byesu_image_routing_policy
+import codex_image_limit_preflight
 import sandbox_launcher_client
 import codex_launcher_runner
 import codex_context_launcher_runner
@@ -45,6 +47,7 @@ assert issubclass(codex_launcher_runner.LauncherTierProviderManager, codex_tier_
 assert issubclass(codex_context_launcher_runner.ContextLauncherTierProviderManager, codex_launcher_runner.LauncherTierProviderManager)
 assert callable(byesu_image_fallback.install_byesu_image_fallback)
 assert callable(byesu_image_routing_policy.install_byesu_image_routing_policy)
+assert callable(codex_image_limit_preflight.install_codex_image_limit_preflight)
 print("HERMES_RUNTIME_IMPORT_GRAPH_OK")
 """
 
