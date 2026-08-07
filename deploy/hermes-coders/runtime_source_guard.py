52a2623d77d3af45b105f0a9f12c746252d46120
Stellmaria <aam.cheshire@gmail.com>
2026-08-07 21:47:05 +0300

    Guard high-resolution runtime module

--- deploy/hermes-coders/runtime_source_guard.py
+++ deploy/hermes-coders/runtime_source_guard.py
@@ -20,6 +20,7 @@ RUNTIME_SOURCES = (
     "byesu_image_fallback.py",
     "byesu_image_routing_policy.py",
     "codex_image_limit_preflight.py",
+    "codex_image_high_res_export.py",
     "codex_launcher_runner.py",
     "codex_context_launcher_runner.py",
     "sandbox_launcher_client.py",
@@ -37,6 +38,7 @@ import codex_image_runner
 import byesu_image_fallback
 import byesu_image_routing_policy
 import codex_image_limit_preflight
+import codex_image_high_res_export
 import sandbox_launcher_client
 import codex_launcher_runner
 import codex_context_launcher_runner
@@ -48,6 +50,7 @@ assert issubclass(codex_context_launcher_runner.ContextLauncherTierProviderManag
 assert callable(byesu_image_fallback.install_byesu_image_fallback)
 assert callable(byesu_image_routing_policy.install_byesu_image_routing_policy)
 assert callable(codex_image_limit_preflight.install_codex_image_limit_preflight)
+assert callable(codex_image_high_res_export.install_codex_image_high_res_export)
 print("HERMES_RUNTIME_IMPORT_GRAPH_OK")
 """
 
