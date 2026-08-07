f0bc44943d154fe8b18901d5c2a95b67895bfe65
Stellmaria <aam.cheshire@gmail.com>
2026-08-07 18:35:39 +0300

    Guard dual Byesu credential runtime source

--- deploy/hermes-coders/runtime_source_guard.py
+++ deploy/hermes-coders/runtime_source_guard.py
@@ -18,6 +18,7 @@ RUNTIME_SOURCES = (
     "codex_tier_runner.py",
     "codex_image_runner.py",
     "byesu_image_fallback.py",
+    "byesu_image_credentials.py",
     "byesu_image_routing_policy.py",
     "codex_image_limit_preflight.py",
     "codex_launcher_runner.py",
@@ -35,6 +36,7 @@ import codex_provider_chain_runner
 import codex_tier_runner
 import codex_image_runner
 import byesu_image_fallback
+import byesu_image_credentials
 import byesu_image_routing_policy
 import codex_image_limit_preflight
 import sandbox_launcher_client
@@ -46,6 +48,7 @@ assert codex_routed_runner.Handler is Handler
 assert issubclass(codex_launcher_runner.LauncherTierProviderManager, codex_tier_runner.AuditedTierProviderManager)
 assert issubclass(codex_context_launcher_runner.ContextLauncherTierProviderManager, codex_launcher_runner.LauncherTierProviderManager)
 assert callable(byesu_image_fallback.install_byesu_image_fallback)
+assert callable(byesu_image_credentials.install_byesu_dual_credentials)
 assert callable(byesu_image_routing_policy.install_byesu_image_routing_policy)
 assert callable(codex_image_limit_preflight.install_codex_image_limit_preflight)
 print("HERMES_RUNTIME_IMPORT_GRAPH_OK")
